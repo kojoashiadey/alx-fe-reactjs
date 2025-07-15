75ab9420d9cb85243b53daf49db9e143feddb0cf
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-07-15 15:43:02 +0000

    Delete alx-react-app-new/Footer.jsx

--- alx-react-app-new/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
-      <p>&copy; 2025 City Explorer. All rights reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
