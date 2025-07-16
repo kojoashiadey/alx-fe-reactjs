8acfba024e4514bcd0124ad5a2a1438b32312085
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-07-16 07:34:09 +0000

    Update UserDetails.jsx

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,17 +1,16 @@
-// src/components/UserProfile.jsx
+// src/components/UserDetails.jsx
 import { useContext } from 'react';
 import UserContext from './UserContext';
 
-const UserProfile = () => {
-  const { name, age, bio } = useContext(UserContext);
+function UserDetails() {
+  const userData = useContext(UserContext);
 
   return (
     <div>
-      <h2>{name}</h2>
-      <p>Age: {age}</p>
-      <p>Bio: {bio}</p>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
-};
+}
 
-export default UserProfile;
+export default UserDetails;
