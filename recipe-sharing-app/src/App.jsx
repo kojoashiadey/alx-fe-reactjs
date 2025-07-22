48833c8c5196c8a3b5a3de9d139a7ee8525fe44a
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-07-22 19:57:54 +0000

    Create App.jsx

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
+import SearchBar from './components/SearchBar';
+import FavoritesList from './components/FavoritesList';
+import RecommendationsList from './components/RecommendationsList';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>🍲 Recipe Sharing App</h1>
+        <nav style={{ marginBottom: '1rem' }}>
+          <Link to="/">Home</Link> | <Link to="/favorites">Favorites</Link> | <Link to="/recommendations">Recommendations</Link>
+        </nav>
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar />
+                <AddRecipeForm />
+                <RecipeList />
+              </>
+            }
+          />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route path="/favorites" element={<FavoritesList />} />
+          <Route path="/recommendations" element={<RecommendationsList />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
