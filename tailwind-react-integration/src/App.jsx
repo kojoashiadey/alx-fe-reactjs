64d2c4bc6919f53baab3f7dfa11fee7310efbbd1
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-08-06 20:31:18 +0000

    Create App.jsx

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,11 @@
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
