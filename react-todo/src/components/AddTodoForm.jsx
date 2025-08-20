0559234543e7a58b3af6f914d940d338cb890e4b
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-08-20 13:35:34 +0000

    Create AddTodoForm.jsx

---
+++ react-todo/src/components/AddTodoForm.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+export default function AddTodoForm({ addTodo }) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+    addTodo(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Add new todo"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
